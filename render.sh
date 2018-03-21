e6d48fd83835074667a4361481880993601d43cf
Misha Chitharanjan <mishac@mishac.com>
2018-03-21 10:56:32 -0400

    templatize dockerfile creation and create php 5.6/7.0/7.2 versions

---
+++ render.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+render() {
+sedStr="
+  s!%%PHP_VERSION%%!$version!g;
+"
+
+sed -r "$sedStr" $1
+}
+
+versions=(5.6 7.0 7.1 7.2)
+for version in ${versions[*]}; do
+  mkdir php-$version
+  render Dockerfile.template > php-$version/Dockerfile
+done
